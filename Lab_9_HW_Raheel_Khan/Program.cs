66803c271bd3dde2108cc24e09f6f7a8d5b2af53
Khan <raheel.khan@tylertech.com>
2019-11-07 09:44:22 -0500

    first commit

---
+++ Lab_9_HW_Raheel_Khan/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_9_HW_Raheel_Khan
+{
+    class Program
+    {
+
+    
+
+
+
+        static void Main(string[] args)
+        {
+            StudentInfo student = new StudentInfo("Drew", "Detroit", "Chicken", "blue");
+
+            List<StudentInfo> studentList = new List<StudentInfo>();
+
+            studentList.Add(student);
+
+            Console.WriteLine("Add new Student");
+
+            bool repeat = true;
+            while (repeat)
+            {
+
+                Console.WriteLine("Give me a new students name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Give me a new students homeTown");
+                string homeTown = Console.ReadLine();
+
+                Console.WriteLine("Give me a new students favoriteFood");
+                string favoriteFood = Console.ReadLine();
+
+                Console.WriteLine("Give me a new students favoriteColor");
+                string favoriteColor = Console.ReadLine();
+
+                StudentInfo newStudent = new StudentInfo(name, homeTown, favoriteFood, favoriteColor);
+
+                studentList.Add(newStudent);
+
+                Console.WriteLine("Continue");
+                string input = Console.ReadLine();
+                if (input.ToLower() != "y")
+                {
+                    repeat = false;
+                }
+            }
+
+            foreach (StudentInfo Student in studentList)
+            {
+                Console.WriteLine($"{Student.Name} {Student.HomeTown} {Student.FavoriteFood} {Student.FavoriteColor}");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Lab_9_HW_Raheel_Khan/StudentInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_9_HW_Raheel_Khan
+{
+    class StudentInfo
+    {
+        public string Name;
+        public string HomeTown;
+        public string FavoriteFood;
+        public string FavoriteColor;
+
+        //default constructor
+        public StudentInfo()
+        {
+
+        }
+
+        //overloaded constructor 
+        public StudentInfo(string name)
+        {
+            Name = name;
+
+        }
+
+        public StudentInfo(string name, string homeTown)
+        {
+            Name = name;
+            HomeTown = homeTown;
+
+
+        }
+
+        public StudentInfo(string name, string homeTown, string favoriteFood, string favoriteColor)
+        {
+            Name = name;
+            HomeTown = homeTown;
+            FavoriteFood = favoriteFood;
+            FavoriteColor = favoriteColor;
+        }
+
+    }
+}
